89f032044d54c54467c3131e5a79c4caa15d8aaf
cnzsb <cnzsb@foxmail.com>
2016-08-13 20:12:02 +0800

    add router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,23 @@
 import Vue from 'vue'
 import App from './App'
+import VueRouter from 'vue-router'
 
-/* eslint-disable no-new */
-new Vue({
-    el: 'body',
-    components: {App},
+Vue.use(VueRouter)
+const router = new VueRouter({
+    hashbang: true,
+    history: false,
+    saveScrollPosition: true,
+    transitionOnLoad: true
 })
+
+router.map({
+    '/': {
+        component: App
+    }
+})
+
+router.redirect({
+    '*': '/'
+})
+
+router.start(App, '#app')

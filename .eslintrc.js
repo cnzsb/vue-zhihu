339c37b8176c8a7d59be4e68fbc7f0aedefcdb0a
cnzsb <cnzsb@foxmail.com>
2016-08-13 18:46:53 +0800

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,22 @@
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
-  },
-  extends: 'airbnb-base',
-  // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
-  // add your custom rules here
-  'rules': {
-    'import/no-unresolved': 0,
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
-  }
+    root: true,
+    parser: 'babel-eslint',
+    parserOptions: {
+        sourceType: 'module'
+    },
+    extends: 'airbnb-base',
+    // required to lint *.vue files
+    plugins: [
+        'html'
+    ],
+    // add your custom rules here
+    'rules': {
+        'import/no-unresolved': 0,
+        indent: ['error', 4],
+        semi: ['error', 'never'],
+        'object-curly-spacing': ['error', 'never'],
+        'comma-dangle': ['error', 'only-multiline'],
+        // allow debugger during development
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    }
 }

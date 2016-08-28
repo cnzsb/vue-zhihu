e01c536ed8ad08216d3c390a432f97c0ae10ff95
cnzsb <cnzsb@foxmail.com>
2016-08-28 18:02:53 +0800

    update profile area

--- src/components/topbar.vue
+++ src/components/topbar.vue
@@ -3,8 +3,8 @@
         <div class="topbar">
             <a class="logo" href="/">知乎</a>
             <div class="search-box">
-                <input type="text" class="search-box-content" placeholder="搜索你感兴趣的内容...">
-                <button type="button" class="search-box-button"></button>
+                <input type="text" class="search-box-content" placeholder="搜索你感兴趣的内容..." v-model="searchContent" @keyup.enter="search">
+                <button type="button" class="search-box-button" @click="search"></button>
             </div>
             <ul class="top-nav">
                 <li><a v-link="{path: '/'}">首页</a></li>
@@ -14,8 +14,8 @@
             </ul>
             <div class="top-nav-profile-wrap">
                 <button type="button" class="top-ask">提问</button>
-                <a href="javascript:;">
-                    <img class="avatar" :src="user.avatarUrl" width="27" height="27">
+                <a class="top-nav-profile-info" href="javascript:;">
+                    <img class="avatar" :src="user.avatarUrl">
                     {{user.name}}
                 </a>
                 <ul class="top-nav-profile">
@@ -29,10 +29,16 @@
     export default {
         data() {
             return {
+                searchContent: '',
                 user: {
                     name: '奔跑的Q丶',
                     avatarUrl: 'http://zhaoshibo.net/avatar.jpg'
                 }
+            }
+        },
+        methods: {
+            search() {
+                window.open(`https://www.zhihu.com/search?type=content&q=${this.searchContent}`, '_blank')
             }
         }
     }
@@ -148,6 +154,73 @@
                         &:active {
                             color: #eee;
                         }
+
+                        &:hover {
+                            color: #fff;
+                        }
+                    }
+                }
+            }
+
+            .top-nav-profile-wrap {
+                float: right;
+                position: relative;
+                min-width: 186px;
+                margin-left: 15px;
+                z-index: 10;
+
+                .top-ask {
+                    width: 66px;
+                    height: 32px;
+                    line-height: 30px;
+                    border: 1px solid #045bb2;
+                    border-radius: 4px;
+                    background: #0e78e7;
+                    background: linear-gradient(to bottom, #0e7bef, #0d73da);
+                    box-shadow: 0 1px 2px rgba(0, 0, 0, .05), inset 0 1px 1px rgba(255, 255, 255, .15);
+                    color: #fff;
+                    font-size: 14px;
+                    text-align: center;
+                    text-shadow: 0 1px 1px rgba(0, 0, 0, .12);
+                    outline: 0;
+                    cursor: pointer;
+                    transition: all .2s ease-in-out;
+                    vertical-align: middle;
+
+                    &:active {
+                        background: #096ed5;
+                        background: linear-gradient(top, #0e7ceb, #0c6fd3);
+                        border: 1px solid #0659ac;
+                        box-shadow: 0 1px 0 rgba(255, 255, 255, .06),
+                                    inset 0 1px 2px rgba(0, 0, 0, .2);
+                    }
+                }
+                .top-nav-profile-info {
+                    display: inline-block;
+                    width: 120px;
+                    height: 45px;
+                    line-height: 45px;
+                    font-size: 14px;
+                    text-shadow: 0 1px 1px rgba(0, 0, 0, .12);
+                    color: #fff;
+                    vertical-align: middle;
+                    text-align: center;
+                    white-space: nowrap;
+                    text-overflow: ellipsis;
+                    overflow: hidden;
+
+                    .avatar {
+                        width: 25px;
+                        height: 25px;
+                        border-radius: 3px;
+                        vertical-align: middle;
+                        border: 1px solid rgba(0, 0, 0, .1);
+                        box-shadow: 0 1px 0 rgba(255, 255, 255, .1);
+                        background-color: transparent;
+                    }
+
+                    &:hover {
+                        background-color: #095fb3;
                     }
                 }
             }
